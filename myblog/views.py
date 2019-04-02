1b1a7d941f693a8b50f7e3c23d18126a17dc223c
ZhuojingXie <llxllxzj@gmail.com>
2019-04-01 22:21:48 -0700

    FrontView Commit

--- myblog/views.py
+++ myblog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.template import loader
+from myblog.models import Post
 
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect, Http404
@@ -12,3 +14,9 @@ def stub_view(request, *args, **kwargs):
         body += "Kwargs:\n"
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    context = {'posts': posts}
+    return render(request, 'list.html', context)
